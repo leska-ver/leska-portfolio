e02c0c610c92754b4f2cce4fc11ea938df4a235c
leska-ver <lyaisan.333@yandex.ru>
2023-08-30 13:19:31 +0500

    leska-portfolio

---
+++ js/leska-portfolio.js
@@ -0,0 +1,46 @@
+document.addEventListener('DOMContentLoaded', function() {  
+  //alert('работает');Ищет ошибки по строчно
+
+
+  // Модальное окно для нескольких окон. Модалка не прокручиваеться.//
+  const activeClass = "modal-active";
+  const buttons = document.querySelectorAll(".modalBtn-js");
+
+  for (let button of buttons) {
+    modalEvent(button);
+  }
+  
+  function closeModal (modal) {
+	 modal.classList.remove(activeClass);
+	 document.body.style.overflow  = '';
+  }
+	  
+  function modalEvent(button) {
+    button.addEventListener("click", (e) => {//(e) - дефолт - чтобы при нажитие на модального окна, модалка не улетала вверх.
+      e.preventDefault();
+
+      const trigger = button.getAttribute("data-modal-trigger");
+      const modal = document.querySelector(`[data-modal=${trigger}]`);
+      const modalContent = modal.querySelector(".modal-container");
+      const close = modal.querySelector(".modal-close");
+           
+      /* --Cтили body при открытие модального окна-- */
+      modal.classList.add('modal-active');		 
+      if (modal.classList.contains(activeClass)) {
+        document.body.style.overflow  = 'hidden';
+      }
+
+      close.addEventListener("click", () =>  {
+		 closeModal (modal); 
+	  });
+      modal.addEventListener("click", () => {
+		 closeModal (modal); 
+	  });
+      modalContent.addEventListener("click", (e) => e.stopPropagation());
+      
+    });
+  };
+
+    
+
+});
